747904c256d225e57c9f37f498f3b7dd9f476637
801cucumber <psyche.delicha@gmail.com>
2016-05-18 17:25:27 +1200

    Add map and panzoom function

---
+++ js/maps.js
@@ -0,0 +1,44 @@
+$(function() {
+  var $section = $('#l1-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+  $section = $('#l2-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+  $section = $('#l3-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+  $section = $('#l4-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+  $section = $('#l5-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+  $section = $('#l6-m');
+  $section.find('.panzoom').panzoom({
+    $zoomIn: $section.find(".zoom-in"),
+    $zoomOut: $section.find(".zoom-out"),
+    $zoomRange: $section.find(".zoom-range"),
+    $reset: $section.find(".reset")
+  });
+});
